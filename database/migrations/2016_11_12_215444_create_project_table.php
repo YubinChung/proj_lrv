d3560aad0d8e41dbb0029a7fc22ee813b3ce6391
cuuuv <ceeuvex@gmail.com>
2016-11-13 12:40:00 +1300

    add table

--- database/migrations/2016_11_12_215444_create_project_table.php
+++ database/migrations/2016_11_12_215444_create_project_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateProjectTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,12 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('projects', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password', 60);
+            $table->integer('user_id')->unsigned();
+			$table->foreign('user_id')->references('id')->on('users');
+            $table->string('name', 20);
+            $table->boolean('public')->default(false);
             $table->timestamps();
         });
     }
@@ -29,6 +30,7 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::drop('users');
+		Schema::drop('projects');
+        Schema::dropIfExists('projects');
     }
 }
